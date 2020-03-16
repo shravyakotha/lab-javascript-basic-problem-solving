c57f2834087c580b705bfb816702dbd15dc0ca9f
unknown <kothasairarun@gmail.com>
2020-03-16 21:42:43 +0530

    updated

--- app.js
+++ app.js
@@ -5,6 +5,11 @@
 // 1.2 Print `"The driver's name is XXXX"`.
 // 1.3 Create a variable `ProGrad-2` with the navigator's name.
 // 1.4 Print `"The navigator's name is YYYY"`.
+var prograd1 = "Deepthi";
+console.log("The driver's name is " + prograd1);
+var prograd2 = "Shravya";
+console.log("The navigator's name is " + prograd2);
+
 
 // Progression 2: Control Statements - 1
 // 2.1. Depending on which name is longer, print:
@@ -12,19 +17,84 @@
 // - It seems that the navigator has the longest name, it has XX characters. or
 // - Wow, you both have equally long names, XX characters!.
 
+
+var check1 = prograd1.length;
+var check2 = prograd2.length;
+if (check1 > check2) {
+    console.log("The driver has the longest name, it has" + check1 + "characters");
+} else if (check1 < check2) {
+    console.log("The navigator has the longest name, it has" + check2 + "characters");
+} else {
+    console.log("Wow, you both have equally long names with " + check1 + " characters");
+}
+
+
 // 2.2. Check if the string contains vowels or not.
 // - If it contains vowels, print the number of name, and also print the vowel letters along with the vowel characters. or
 // - print no vowels
+
+var count = 0;
+
+for (var i = 1; i <= prograd1.length; i++) {
+    v = prograd1[i];
+    if (v == "A" || v == "E" || v == "I" || v == "O" || v == "U" ||
+        v == "a" || v == "e" || v == "i" || v == "o" || v == "u") {
+        count++;
+        console.log(v);
+    }
+}
+if (count == 0) {
+    console.log("no vowels");
+} else {
+    console.log(count);
+}
 
 // 2.3. Check if the string contains uppercase and lowercase characters Xx
 // - Print the number of upper case characters
 // - Print the number of lower case characters
 
+countu = 0;
+countl = 0;
+for (var j = 1; j <= prograd1.length; j++) {
+
+    if (prograd1[j].toUpperCase() == prograd1[j]) {
+        countu++;
+        console.log("The Upper case letter is " + prograd1[j]);
+    } else {
+        countl++;
+        console.log("The Lower case letter is " + prograd1[j]);
+    }
+
+}
+console.log(countu);
+console.log(countl);
+
+
 
 // Progression 3: Control Statements - 2
 // 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "ProGrad"
 
+var s= "Faceprep";
+var i=0;
+var a=[];
+for(i=0;i<=s.length-1;i++)
+{
+    a[i]=s[i.toUpperCase()=""
+}
+console.log(a);
+for(i=s.length-1=i>=0;i--)
+{
+    reversed+=str;
+    console.log(i);
+}
+}
+
 // 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "darGorP"
+
+function reverseString(str){
+    return str.split("").reverse().join("");
+}
+reverseString("ProGrad");
 
 // 3.3 Merge both the characters such that driver is followed by Navigator like "ProGrad FACEPrep"
 // - Now bring the FACEPrep to the start and send ProGrad to the back like "FACEPrep ProGrad"
@@ -53,3 +123,26 @@
 // "Was it a car or a cat I saw?" and "No 'x' in Nixon".
 
 // Hint: If you use Google to help you to find solution to this iteration, you might run into some solutions that use advanced string or array methods (such as join(), reverse(), etc.). However, try to apply the knowledge you currently have since you can build pretty nice solution with just using for loop, if-else statements with some break and continue... Just sayin' 
+
+
+
+
+var str = "prograd";
+var str1=str.toUpperCase();
+for (var k = 0; k < str1.length; k++) {
+    console.log(str1[k] + " ");
+}
+
+function reverseString(passs) {
+    var newString = "";
+    for (var i = prograd2.length - 1; i--) {
+        newString += str[i];
+    }
+    return newString;
+
+}
+reverseString("Shravya");
+
+//What is the problem
+// i just nw had my dnr nad satrtd my work Okay
+//Open story time lab
